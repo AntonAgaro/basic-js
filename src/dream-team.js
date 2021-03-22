29f9f5bfe1008f27c813aa1b76e3a8995994c723
AntonAgaro <75292851+AntonAgaro@users.noreply.github.com>
2021-03-22 17:58:19 +0400

    feat: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(members) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let letters = '';
+  for (let i = 0; i < members.length; i++) {
+    if ((typeof members[i]) == 'string') {
+    letters += members[i][0];
+    };
+  }
+  if (letters === '') {
+    return false;
+  } else {
+  return letters.split('').sort().join('');
+  }
 };
