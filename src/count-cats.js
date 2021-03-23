8d1b4c92a5e752954b4dc98294ea82c0d10f1c52
AntonAgaro <75292851+AntonAgaro@users.noreply.github.com>
2021-03-23 20:24:54 +0400

    feat:solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,8 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(backyard) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
   let arr = [].concat(...backyard);
   let count = 0;
   for(let i = 0; i < arr.length; i++) {
